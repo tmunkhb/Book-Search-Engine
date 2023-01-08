d91ca7709dfd2de0f4b154ed9ab379421cbbdd70
tmunkhb <tmunkhb@gmail.com>
2023-01-07 19:57:58 -0500

    created typeDefs queries

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    input saveBookInput {
+        authors: [String]!
+        description: String!
+        title: String!
+        bookId: String!
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: saveBookInput): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
