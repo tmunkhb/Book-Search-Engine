4eca7038493d05a80634306e16361b2ff63860e0
tmunkhb <tmunkhb@gmail.com>
2023-01-07 20:25:42 -0500

    updated codes for mutation.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,61 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER = gql ``;
-export const ADD_USER = gql ``;
-export const SAVE_BOOK = gql ``;
-export const REMOVE_BOOK = gql ``;
+export const LOGIN_USER = gql `
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }    
+`;
+
+export const ADD_USER = gql `
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql `
+    mutation saveBook($input: saveBookInput!) {
+        saveBook(input: $input) {
+            _id
+            username
+            bookCount
+            savedBooks {
+               authors
+               bookId
+               description
+               image
+               link
+               title
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql `
+    removeBook(bookId: $bookId) {
+            _id
+            username
+            bookCount
+            savedBooks {
+                authors
+                bookId
+                description
+                image
+                link
+                title
+            }
+        }
+    }
+
+`;
