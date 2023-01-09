66c088c87e5b44e15741931b101010c9bdefcdf0
tmunkhb <tmunkhb@gmail.com>
2023-01-08 19:52:35 -0500

    update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Google-books', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Google-books', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
